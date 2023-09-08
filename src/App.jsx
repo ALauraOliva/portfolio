865975eeca86860cb45e4d8824bad043ab45abcd
Andrea Laura <andrea_carol00@hotmail.com>
2023-09-08 17:50:42 -0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.scss"
+import { Home } from "./components/Home/Home"
+import "./scss/base/_body.scss"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>Hola mundo</div>
+      <Home />
     </>
   )
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import "../../scss/components/_home.scss"
+
+export const Home = () => {
+  return <div className="container">Home</div>
+}
