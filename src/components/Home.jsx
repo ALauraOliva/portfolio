e4e80c4528e75c0645f7199702048765f2e763f4
Andrea Laura <andrea_carol00@hotmail.com>
2023-09-10 22:54:10 -0500

    --

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Home = () => {
   return (
     <header className="home_container">
-      <h1>Andrea Laura</h1>
+      <h1>Hi, Andrea Laura</h1>
     </header>
   )
 }
