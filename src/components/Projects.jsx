be391f120b38873471bf7d83b604b7874ba58bae
Andrea Laura <andrea_carol00@hotmail.com>
2023-09-18 17:18:29 -0500

    animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,29 @@
 import React from "react"
+import { AiFillHtml5 } from "react-icons/ai"
+import { SiCss3 } from "react-icons/si"
+import { BiLogoReact } from "react-icons/bi"
 
 export const Projects = () => {
+  const listProjects = [<AiFillHtml5 />, <SiCss3 />, <BiLogoReact />]
+  const deg = 30 / listProjects.length
+
   return (
     <section id="projects" className="projects_section">
-      <h1>projects</h1>
+      <div className="projects_nav">
+        <h1>projects</h1>
+        <div className="projects_spinning">
+          <p>
+            {listProjects.map((project, index) => (
+              <span
+                key={index}
+                style={{ transform: `rotate(${deg * index}deg)` }}
+              >
+                {project}
+              </span>
+            ))}
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
