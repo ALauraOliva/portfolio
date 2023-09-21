88eb0a70c28589f1c0971a98c294a35d627ec645
Andrea Laura <andrea_carol00@hotmail.com>
2023-09-21 18:11:41 -0500

    almost done projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,16 +6,44 @@ export const Project = ({ project }) => {
       id={project.name}
       className={`project_section project_${project.name.replace(/\s+/g, "_")}`}
     >
-      <h1>{project.name}</h1>
-      <article className="project_scroller">
-        <p>{project.resume}</p>
-        <div>
-          <h2>Role</h2>
-          {project.role}
-          <h2>Tech Stack</h2>
-          {project.tech}
-        </div>
-      </article>
+      <div className="project_slides">
+        <input
+          type="radio"
+          name="radio"
+          id={`project_${project.name.replace(/\s+/g, "_")}1`}
+          checked
+        />
+        <input
+          type="radio"
+          name="radio"
+          id={`project_${project.name.replace(/\s+/g, "_")}2`}
+        />
+
+        <article className="project_slide slide_checked">
+          <h1>{project.name}</h1>
+          <div className="project_content">
+            <p>{project.resume}</p>
+            <div className="project_subContent">
+              <h2>Role</h2>
+              {project.role}
+              <h2>Tech Stack</h2>
+              {project.tech}
+            </div>
+          </div>
+        </article>
+        <article className="project_slide">blah</article>
+      </div>
+
+      <div className="project_navigation">
+        <label
+          className="bar"
+          for={`project_${project.name.replace(/\s+/g, "_")}1`}
+        ></label>
+        <label
+          className="bar"
+          for={`project_${project.name.replace(/\s+/g, "_")}2`}
+        ></label>
+      </div>
     </section>
   )
 }
